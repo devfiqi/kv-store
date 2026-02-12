483bf8293a598fd745021c99f5152c0de5f183ca
salmanfiqi <salmanfiqi@gmail.com>
2026-02-12 00:43:57 -0600

    Fix ServerSocket not closing by wrapping in try-with-resources

--- src/main/java/com/kvstore/KVServer.java
+++ src/main/java/com/kvstore/KVServer.java
@@ -13,38 +13,38 @@ public class KVServer {
     }
 
     public void start() throws IOException{
-        ServerSocket server = new ServerSocket(port);
+        try (ServerSocket server = new ServerSocket(port)) {
+            while (true) {
+                Socket client = server.accept();
 
-        while (true) {
-            Socket client = server.accept();
+                // read from client
+                BufferedReader in = new BufferedReader(
+                    new InputStreamReader(client.getInputStream())
+                );
 
-            // read from client
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(client.getInputStream())
-            );
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
 
-            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                String line = in.readLine();
 
-            String line = in.readLine();
+                String[] cmd = line.split(" ");
 
-            String[] cmd = line.split(" ");
+                if (cmd[0].equals("PUT")) {
+                    store.put(cmd[1], cmd[2]);
+                    out.println("OK");
+                }
 
-            if (cmd[0].equals("PUT")) {
-                store.put(cmd[1], cmd[2]);
-                out.println("OK");
+                if (cmd[0].equals("GET")) {
+                    String value = store.get(cmd[1]);
+                    out.println(value);
+                }
+
+                if (cmd[0].equals("DELETE")) {
+                    store.delete(cmd[1]);
+                    out.println("OK");
+                }
+
+                client.close();
             }
-
-            if (cmd[0].equals("GET")) {
-                String value = store.get(cmd[1]);
-                out.println(value);
-            }
-
-            if (cmd[0].equals("DELETE")) {
-                store.delete(cmd[1]);
-                out.println("OK");
-            }
-
-            client.close();
         }
     }
 
